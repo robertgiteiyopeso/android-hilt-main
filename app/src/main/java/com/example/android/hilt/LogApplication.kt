639980a0ecff6a7ede60b46a09008bf27e22a897
Robert Gitei <robert.gitei@yopeso.com>
2022-08-23 15:05:11 +0300

    8. Providing interfaces with @Binds

--- app/src/main/java/com/example/android/hilt/LogApplication.kt
+++ app/src/main/java/com/example/android/hilt/LogApplication.kt
@@ -22,10 +22,4 @@ import dagger.hilt.android.HiltAndroidApp
 @HiltAndroidApp
 class LogApplication : Application() {
 
-    lateinit var serviceLocator: ServiceLocator
-
-    override fun onCreate() {
-        super.onCreate()
-        serviceLocator = ServiceLocator(applicationContext)
-    }
 }
